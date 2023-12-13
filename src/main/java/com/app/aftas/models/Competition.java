03260a358f6a1421591af59cef2063149b64f28d
HIND-HANSALI <hindhsl111@gmail.com>
2023-12-13 14:25:41 +0100

    add Competition entity

--- src/main/java/com/app/aftas/models/Competition.java
+++ src/main/java/com/app/aftas/models/Competition.java
@@ -1,7 +1,18 @@
 package com.app.aftas.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -14,14 +25,48 @@ public class Competition {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String code;
-    private String date;
-    private String startTime;
-    private String endTime;
-    private int numberOfParticipants;
+
+    @NotNull(message = "Name cannot be null")
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @NotNull(message = "Start time cannot be null")
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime startTime;
+
+    @NotNull(message = "Start time cannot be null")
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime endTime;
+
+    @NotNull(message = "Location cannot be null")
+    @Size(min = 2, max = 50, message = "Location must be between 2 and 50 characters")
     private String location;
-    private int amountOfFish;
+
+    @NotNull(message = "Amount cannot be null")
+    @Min(value = 0, message = "Amount must be greater than 0")
+    private int amount;
+
+    @NotNull(message = "Total member cannot be null")
+    @Min(value = 0, message = "Total member must be greater than 0")
+    private int totalMember;
 
     @OneToMany(mappedBy = "competition")
     private List<Ranking> rankings;
+
+    @OneToMany(mappedBy = "competition")
+    private List<Hunting> huntings;
+
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date updatedAt;
 }
